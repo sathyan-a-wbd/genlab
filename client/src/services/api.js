989b46938ea2e37aa23b7ba6bb5a6f5d89c08bfa
sathyan-a-wbd <sathyana3011@gmail.com>
2026-08-09 09:45:50 +0530

    Fix: Cors fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://genlab-1.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://genlab-2.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
